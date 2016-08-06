cad2d99e8bc0a693886fcc4f1ab2186894223486
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2016-08-05 21:54:54 -0300

    Implemented delete and delete tests

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -9,7 +9,12 @@ defmodule KV.BucketTest do
     test "stores values by key", %{bucket: bucket} do
         assert KV.Bucket.get(bucket, "milk") == nil
 
-        KV.Bucket.put(bucket, "milk", 3)
+        # add milk kv
+        KV.Bucket.put bucket, "milk", 3
         assert KV.Bucket.get(bucket, "milk") == 3
+
+        # remove milk
+        KV.Bucket.delete bucket, "milk"
+        assert KV.Bucket.get(bucket, "milk") == nil
     end
 end
